eaa308a404f19680aaafc384ef44fb98d8865460
Juan Diego Guerra <jdxelric@gmail.com>
2023-04-27 01:46:55 -0500

    Update app.js
    Created event when Adding a book with the "add" button

--- js/app.js
+++ js/app.js
@@ -47,5 +47,27 @@ function displayBook() {
   bookListDiv.appendChild(bookList);
 }
 
+const form = document.querySelector('#form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const title = document.getElementById('book-title').value;
+  // Capitalize first letter of title value
+  const titleUC = title.charAt(0).toUpperCase() + title.slice(1);
+  const author = document.getElementById('book-author').value;
+  const arrAuthor = author.split(' ');
+  // Capitalize first letter of each word of author value
+  for (let i = 0; i < arrAuthor.length; i += 1) {
+    arrAuthor[i] = arrAuthor[i].charAt(0).toUpperCase() + arrAuthor[i].slice(1);
+  }
+  const authorUC = arrAuthor.join(' ');
+  if ((title && author) !== '') {
+    const book = new Book(titleUC, authorUC);
+    const library = new Library(book);
+    library.addBook();
+    form.reset();
+    displayBook();
+  }
+});
+
 
 

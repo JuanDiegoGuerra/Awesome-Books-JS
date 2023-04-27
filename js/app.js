f5eb187d60ea3dc3d1b84d4004242d549774252a
Juan Diego Guerra <jdxelric@gmail.com>
2023-04-27 01:46:33 -0500

    Update app.js
    Created DisplayBook function

--- js/app.js
+++ js/app.js
@@ -18,7 +18,34 @@ class Library {
   }
 }
 
-
+function displayBook() {
+  const bookListDiv = document.querySelector('.book-list');
+  bookListDiv.innerHTML = '';
+  const bookList = document.createElement('ul');
+  Books.forEach((element, idx) => {
+    const li = document.createElement('li');
+    li.setAttribute('id', element.id);
+    li.innerHTML = `
+    <p> "${element.title}" By ${element.author} </p> 
+    `;
+    const removeBtn = document.createElement('button');
+    removeBtn.innerHTML = 'Remove';
+    removeBtn.addEventListener('click', () => {
+      const library = new Library(element);
+      library.removeBook();
+      li.remove();
+      displayBook();
+    });
+    li.appendChild(removeBtn);
+    if (idx % 2 === 0) {
+      li.classList.add('dark');
+    } else {
+      li.classList.add('light');
+    }
+    bookList.appendChild(li);
+  });
+  bookListDiv.appendChild(bookList);
+}
 
 
 
